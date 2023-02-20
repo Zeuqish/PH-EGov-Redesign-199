3e55ff2f37552b874949e5e7cca0d1dda6c386e7
Kiyan Vasquez <krvasquez@up.edu.ph>
2023-02-20 15:27:33 +0800

    Create ui-block foundation

--- src/app/ui/block/block.component.ts
+++ src/app/ui/block/block.component.ts
@@ -2,7 +2,11 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'ui-block',
-  template: `<div></div>`,
+  template: `
+    <div class="block-wrapper">
+      <div class="block-padder"><ng-content></ng-content></div>
+    </div>
+  `,
   styleUrls: ['./block.component.scss'],
 })
 export class BlockComponent {
