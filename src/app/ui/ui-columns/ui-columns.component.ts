8c5dce2ee11e10d7a419191b4afd5f8f8bf9ae8a
Kiyan Vasquez <krvasquez@up.edu.ph>
2023-02-27 11:57:56 +0800

    create columns and container component

--- src/app/ui/ui-columns/ui-columns.component.ts
+++ src/app/ui/ui-columns/ui-columns.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'ui-columns',
   template: `
-    <div class="columns-wrapper">
+    <div class="columns-autofit-wrapper">
       <ng-content></ng-content>
     </div>
   `,
