27d1bb64a8bd71b8ffaaf266059669854e48d9c5
Kiyan Vasquez <krvasquez@up.edu.ph>
2023-02-28 21:21:50 +0800

    Implement Text Block

--- src/app/blocks/block-text/block-text.component.ts
+++ src/app/blocks/block-text/block-text.component.ts
@@ -1,20 +1,33 @@
 import { Component, Input } from '@angular/core';
-
+import { HeadingSize, SplitColumnsPosition } from 'src/app/ui/typedefs';
 @Component({
   selector: 'block-text',
   template: `
     <ui-block>
-      <div *ngIf="true">
-        <ui-columns-split-type>
-          <ui-text>
-            <ng-content></ng-content>
-          </ui-text>
-        </ui-columns-split-type>
-      </div>
+      <ui-container>
+        <ng-content select="ui-heading"></ng-content>
+      </ui-container>
+      <ui-columns-split-type [split]="this.splitType">
+        <ui-container *ngIf="this.splitType == SplitColumnsPosition.LEFT">
+          <ng-content select="[left]"></ng-content>
+        </ui-container>
+        <ui-container>
+          <ng-content select="ui-text"></ng-content>
+        </ui-container>
+        <ui-container *ngIf="this.splitType == SplitColumnsPosition.RIGHT">
+          <ng-content select="[right]"></ng-content>
+        </ui-container>
+      </ui-columns-split-type>
     </ui-block>
   `,
   styleUrls: ['./block-text.component.scss'],
 })
 export class BlockTextComponent {
-  @Input() splitType: string = '';
+  @Input() splitType: SplitColumnsPosition = SplitColumnsPosition.LEFT;
+
+  @Input() headingContent: string = '';
+  @Input() textContent: string = '';
+
+  HeadingSize = HeadingSize;
+  SplitColumnsPosition = SplitColumnsPosition;
 }
