da78edfa540eb4d07eafbad82071ff4d41ec356a
Kiyan Vasquez <krvasquez@up.edu.ph>
2023-03-02 21:22:25 +0800

    allow optional background image on ui-block

--- src/app/ui/ui-block/ui-block.component.ts
+++ src/app/ui/ui-block/ui-block.component.ts
@@ -4,13 +4,39 @@ import { Component, Input } from '@angular/core';
   selector: 'ui-block',
   template: `
     <div class="block-wrapper">
-      <div class="block-column-item">
+      <div
+        class="block-column-item"
+        [style]="getColumnSpan()"
+        style="grid-row:1;"
+      >
         <ng-content></ng-content>
+      </div>
+
+      <div
+        *ngIf="this.optionalGrid"
+        class="block-column-item-optional"
+        [style]="getOptionalColumnSpan()"
+        style="grid-row:1;"
+      >
+        <ng-content select="[optional]"></ng-content>
       </div>
     </div>
   `,
   styleUrls: ['./ui-block.component.scss'],
 })
 export class UIBlockComponent {
-  @Input() label: string = '';
+  @Input() colStart: number = 3;
+  @Input() colEnd: number = 11;
+
+  @Input() optionalGrid: boolean = false;
+  @Input() optionalColStart: number = 3;
+  @Input() optionalColEnd: number = 11;
+
+  getColumnSpan() {
+    return `--col-start: ${this.colStart}; --col-end: ${this.colEnd}`;
+  }
+
+  getOptionalColumnSpan() {
+    return `--col-start-opt: ${this.optionalColStart}; --col-end-opt: ${this.optionalColEnd}`;
+  }
 }
