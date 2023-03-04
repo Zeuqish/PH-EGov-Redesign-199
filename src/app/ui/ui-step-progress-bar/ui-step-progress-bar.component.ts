ea0fb1fda8fd8c15cdcf25c3e4730ec690823de2
Aira <aealoveros@up.edu.ph>
2023-03-04 16:44:45 +0800

    Implement Progress Bar without label

--- src/app/ui/ui-step-progress-bar/ui-step-progress-bar.component.ts
+++ src/app/ui/ui-step-progress-bar/ui-step-progress-bar.component.ts
@@ -3,20 +3,19 @@ import { Component, ViewEncapsulation} from '@angular/core';
 @Component({
   selector: 'ui-step-progress-bar',
   template: `
-  <html>
     <div class="container">
-      <span *ngFor="let item of status;let i = index">
-        <span class="circle-desc" [class.circle-desc-active]="currentStatus == item">{{ item }}</span>
-        <span *ngIf="completedStatus.indexOf(item)!=-1; else incompleteStatus" >
-            <span class="circles" [class.completed]="true">{{i+1}}</span>
+        <span *ngFor="let item of status;let i = index">
+          <span class="circle-desc" [class.circle-desc-active]="currentStatus == item">{{ item }}</span>
+          <span *ngIf="completedStatus.indexOf(item)!=-1; else incompleteStatus" >
+              <span class="circles" [class.completed]="true">{{i+1}}</span>
+              <span class="bar" *ngIf="(i != status.length-1)"></span>
+          </span>
+          <ng-template #incompleteStatus>
+            <span class="circles" [class.active]="currentStatus == item">{{i+1}}</span>
             <span class="bar" *ngIf="(i != status.length-1)"></span>
+          </ng-template>
         </span>
-        <ng-template #incompleteStatus>
-          <span class="circles" [class.active]="currentStatus == item">{{i+1}}</span>
-          <span class="bar" *ngIf="(i != status.length-1)"></span>
-        </ng-template>
-      </span>
-    </div>
+      </div>
   `,
   styleUrls: ['./ui-step-progress-bar.component.scss'],
   encapsulation:ViewEncapsulation.ShadowDom
