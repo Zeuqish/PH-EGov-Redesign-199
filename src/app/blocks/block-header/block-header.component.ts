52a0b1b9349598094515e44c2f54834914976ad1
Kiyan Vasquez <krvasquez@up.edu.ph>
2023-03-16 14:52:56 +0800

    center header text part

--- src/app/blocks/block-header/block-header.component.ts
+++ src/app/blocks/block-header/block-header.component.ts
@@ -8,13 +8,13 @@ import {
 @Component({
   selector: 'block-header',
   template: `
-    <ui-block class="center-content" [colStart]="4" [colEnd]="10">
+    <ui-block [colStart]="4" [colEnd]="10">
       <ui-columns-split-type [splitSize]="SplitColumnsSize.SMALL">
         <div style="width:100px; margin:auto;">
           <ui-image [imageShape]="ImageShape.CIRCLE" [imageSrc]="this.imageSrc">
           </ui-image>
         </div>
-        <ui-container>
+        <div class="center-container">
           <ui-heading [headingSize]="30" class="header-text">
             {{ this.headingContent }}
           </ui-heading>
@@ -23,7 +23,7 @@ import {
           <ui-text class="sub-text">
             The Official Website of the {{ this.textContent }}
           </ui-text>
-        </ui-container>
+        </div>
       </ui-columns-split-type>
     </ui-block>
   `,
