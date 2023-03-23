3c0b4b2f0e8bab57b3690e2e9f24360cd95b4547
Kiyan Vasquez <krvasquez@up.edu.ph>
2023-03-23 18:27:12 +0800

    added links to sitemap

--- src/app/blocks/block-sitemap/block-sitemap.component.ts
+++ src/app/blocks/block-sitemap/block-sitemap.component.ts
@@ -11,49 +11,89 @@ import {
 @Component({
   selector: 'block-sitemap',
   template: `
-  <div class="site-map">
-    <ui-block class="center-content">
-    <ui-columns style="center-content grid-row">
-        <ui-container>
-          <ui-heading [headingSize]="HeadingSize.HEADING_SIX"><div class="site-heading">Services</div></ui-heading>
-          <ul class="site-map-list">
-            <li><ui-link>Business Permit</ui-link></li>
-            <li><ui-link>Education Scholarships</ui-link></li>
-            <li><ui-link>Free COVID Assitance</ui-link></li>
-            <li><ui-link>Provincial ID Application</ui-link></li>
-          </ul>
-        </ui-container>
-        <ui-container>
-          <ui-heading [headingSize]="HeadingSize.HEADING_SIX"><div class="site-heading">About</div></ui-heading>
-          <ul class="site-map-list">
-            <li><ui-link>Mission and Vision</ui-link></li>
-            <li><ui-link>History</ui-link></li>
-            <li><ui-link>Governors</ui-link></li>
-          </ul>
-        </ui-container>
-        <ui-container>
-          <ui-heading [headingSize]="HeadingSize.HEADING_SIX"><div class="site-heading">Government Links</div></ui-heading>
-          <ul class="site-map-list">
-            <li><a href="">Official Gazette</a></li>
-            <li><a href="">Office of the President</a></li>
-            <li><a href="">Senate of the Philippines</a></li>
-            <li><a href="">House of Representatives</a></li>
-            <li><a href="">Supreme Court</a></li>
-            <li><a href="">Court of Appeals</a></li>
-            <li><a href="">Sandigan Bayan</a></li>
-          </ul>
-        </ui-container>
-        <ui-container>
-          <ui-heading [headingSize]="HeadingSize.HEADING_SIX"><div class="site-heading">Contact Us</div></ui-heading>
-          <ul class="site-map-text">
-            <li>Tel. No. 1234-5678</li>
-            <li>Circumferencial Road cor. P. Oliveros St.,Ynares Center Complex,
-              Antipolo City, 1870.</li>
-          </ul>
-        </ui-container>
-      </ui-columns>
-    </ui-block>
-  </div>
+    <div class="site-map">
+      <ui-block class="center-content">
+        <ui-columns style="center-content grid-row">
+          <ui-container>
+            <ui-heading [headingSize]="HeadingSize.HEADING_SIX"
+              ><div class="site-heading">Services</div></ui-heading
+            >
+            <ul class="site-map-list">
+              <li>
+                <ui-link [url]="'/services/business-permit'">
+                  Business Permit
+                </ui-link>
+              </li>
+              <li>
+                <ui-link [url]="'/services/education'">
+                  Education Scholarships
+                </ui-link>
+              </li>
+              <li>
+                <ui-link [url]="'/services/covid'">
+                  Free COVID Assitance
+                </ui-link>
+              </li>
+              <li>
+                <ui-link [url]="'/services/id'">
+                  Provincial ID Application
+                </ui-link>
+              </li>
+            </ul>
+          </ui-container>
+          <ui-container>
+            <ui-heading [headingSize]="HeadingSize.HEADING_SIX">
+              <div class="site-heading">About</div>
+            </ui-heading>
+            <ul class="site-map-list">
+              <li>
+                <ui-link [url]="'/about'">Mission and Vision</ui-link>
+              </li>
+              <li><ui-link [url]="'/about/history'">History</ui-link></li>
+              <li><ui-link [url]="'/about/governors'">Governors</ui-link></li>
+            </ul>
+          </ui-container>
+          <ui-container>
+            <ui-heading [headingSize]="HeadingSize.HEADING_SIX">
+              <div class="site-heading">Government Links</div>
+            </ui-heading>
+            <ul class="site-map-list">
+              <li><a href="https://www.gov.ph/">Official Gazette</a></li>
+              <li>
+                <a href="http://www.president.gov.ph/"
+                  >Office of the President</a
+                >
+              </li>
+              <li>
+                <a href="http://www.ovp.gov.ph/">Senate of the Philippines</a>
+              </li>
+              <li>
+                <a href="https://www.congress.gov.ph/">
+                  House of Representatives
+                </a>
+              </li>
+              <li><a href="https://sc.judiciary.gov.ph/">Supreme Court</a></li>
+              <li>
+                <a href="https://ca2.judiciary.gov.ph/">Court of Appeals</a>
+              </li>
+              <li><a href="https://sb.judiciary.gov.ph/">Sandigan Bayan</a></li>
+            </ul>
+          </ui-container>
+          <ui-container>
+            <ui-heading [headingSize]="HeadingSize.HEADING_SIX"
+              ><div class="site-heading">Contact Us</div></ui-heading
+            >
+            <ul class="site-map-text">
+              <li>Tel. No. 1234-5678</li>
+              <li>
+                Circumferencial Road cor. P. Oliveros St.,Ynares Center Complex,
+                Antipolo City, 1870.
+              </li>
+            </ul>
+          </ui-container>
+        </ui-columns>
+      </ui-block>
+    </div>
   `,
   styleUrls: ['./block-sitemap.component.scss'],
 })
